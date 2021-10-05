f6f688e789d88a1ca0e0cf0d1b8ac25698538849
Abdoulaye Dia <63371699+Beelzebvb@users.noreply.github.com>
2021-10-05 14:50:19 +0200

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "@expo/vector-icons";
+import Home from "./src/screens/home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="dark-content" />
+      <Home></Home>
     </View>
   );
 }
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
